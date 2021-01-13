711642d5923ccfabdbba49e87e5d9128c22cf435
Bgar13 <68516148+Bgar13@users.noreply.github.com>
2021-01-12 16:41:35 -0800

    homework

---
+++ 01-java-fundamentals/01-first-java-program/FirstJavaProgram.java
@@ -0,0 +1,5 @@
+public class FirstJavaProgram{
+    public static void main(String[] args){
+        System.out.println("My name is Brenda \n" + "I am 27 years old \n"+ "My hometown is the Bay Area\n");
+    }
+}
